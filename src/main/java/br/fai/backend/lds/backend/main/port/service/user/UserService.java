aa5da58cedfcbf2853a42df8513ebfaeafc7c104
Mateus Boche <mateussenaisrs@gmail.com>
2024-08-27 00:13:45 -0300

    feat: criacao da interface de userservice na qual aglomera as interfaces que fazem sentido para user

---
+++ src/main/java/br/fai/backend/lds/backend/main/port/service/user/UserService.java
@@ -0,0 +1,7 @@
+package br.fai.backend.lds.backend.main.port.service.user;
+
+import br.fai.backend.lds.backend.main.domain.UserModel;
+import br.fai.backend.lds.backend.main.port.service.crud.CrudService;
+
+public interface UserService extends CrudService<UserModel>, UpdatePasswordService, ReadByEmailService{
+}
--- src/main/java/br/fai/backend/lds/backend/main/service/user/UserServiceImpl.java
+++ src/main/java/br/fai/backend/lds/backend/main/service/user/UserServiceImpl.java
@@ -2,40 +2,46 @@ package br.fai.backend.lds.backend.main.service.user;
 
 import br.fai.backend.lds.backend.main.domain.UserModel;
 import br.fai.backend.lds.backend.main.port.service.crud.CrudService;
+import br.fai.backend.lds.backend.main.port.service.user.ReadByEmailService;
 import br.fai.backend.lds.backend.main.port.service.user.UpdatePasswordService;
+import br.fai.backend.lds.backend.main.port.service.user.UserService;
 
 import java.util.List;
 
-public class UserServiceImpl implements CrudService<UserModel> , UpdatePasswordService {
+public class UserServiceImpl implements UserService {
 
+  @Override
+  public int create(UserModel entity) {
+    return 0;
+  }
 
-    @Override
-    public int create(UserModel entity) {
-        return 0;
-    }
+  @Override
+  public void delete(int id) {
 
-    @Override
-    public void delete(int id) {
+  }
 
-    }
+  @Override
+  public UserModel findById(int id) {
+    return null;
+  }
 
-    @Override
-    public UserModel findById(int id) {
-        return null;
-    }
+  @Override
+  public List<UserModel> findAll() {
+    return null;
+  }
 
-    @Override
-    public List<UserModel> findAll() {
-        return null;
-    }
+  @Override
+  public void update(int id, UserModel entity) {
 
-    @Override
-    public void update(int id, UserModel entity) {
+  }
 
-    }
+  @Override
+  public boolean updatePassword(int id, String oldPassword, String newPassword) {
+    return false;
+  }
 
-    @Override
-    public boolean updatePassword(int id, String oldPassword, String newPassword) {
-        return false;
-    }
+  @Override
+  public UserModel findByEmail(String email) {
+    return null;
+  }
 }
